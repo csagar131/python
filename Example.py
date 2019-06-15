55de4c859ba99bfd26832f67295335e53dbf6f6d
Ubuntu <ubuntu@ip-172-31-9-246.ap-south-1.compute.internal>
2019-06-15 10:22:15 +0530

    scraping using tor browser

---
+++ Example.py
@@ -0,0 +1,7 @@
+from ConnectionManager import ConnectionManager
+
+cm = ConnectionManager()
+for j in range(5):
+    for i in range(3):
+        print ("\t\t" + cm.request("http://icanhazip.com/").read())
+    cm.new_identity()
